91a3674149b6dca07414f201f7343ae394f455f9
Martin Ebel <martin_ebel@icloud.com>
2022-04-28 20:34:02 +0200

    Added loop for estimators

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -4,18 +4,22 @@ from TaxiFareModel.encoders import DistanceTransformer, TimeFeaturesEncoder
 from TaxiFareModel.utils import compute_rmse
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
-from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import cross_validate
 from memoized_property import memoized_property
 import mlflow
 from mlflow.tracking import MlflowClient
+import joblib
 
 class Trainer():
-    def __init__(self, X, y):
+    def __init__(self, X, y, estimator):
         """
             X: pandas DataFrame
             y: pandas Series
         """
         self.pipeline = None
+        self.estimator = estimator
+        self.val_score = None
+        self.cv_score = None
         self.X = X
         self.y = y
         self.MLFLOW_URI = "https://mlflow.lewagon.ai/"
@@ -39,7 +43,7 @@ class Trainer():
 
         self.pipeline=Pipeline([
             ('preproc', preproc_pipe),
-            ('linear_model', LinearRegression())])
+            ('linear_model', self.estimator)])
 
 
     def run(self):
@@ -50,7 +54,18 @@ class Trainer():
     def evaluate(self, X_test, y_test):
         """evaluates the pipeline on df_test and return the RMSE"""
         y_pred = self.pipeline.predict(X_test)
-        return compute_rmse(y_pred, y_test)
+        self.val_score = compute_rmse(y_pred, y_test)
+        self.cv_score = cross_validate(self.pipeline, X_test, y_test,
+                                       cv=5)['test_score'].mean()
+
+
+    def save_model(self):
+        """ Save the trained model into a model.joblib file """
+        self.evaluate(X_test,y_test)
+        joblib.dump(self.pipeline, 'pipeline.joblib')
+        trainer.mlflow_log_param('models',self.estimator)
+        trainer.mlflow_log_metric('val_score', self.val_score)
+        trainer.mlflow_log_metric('cv_score', self.cv_score)
 
     @memoized_property
     def mlflow_client(self):
@@ -77,14 +92,15 @@ class Trainer():
 if __name__ == "__main__":
     from sklearn.model_selection import train_test_split
     from TaxiFareModel.data import get_data, clean_data
-    N = 10_000
-    df = get_data(nrows=N)
-    df = clean_data(df)
-    y = df["fare_amount"]
-    X = df.drop("fare_amount", axis=1)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
-    trainer = Trainer(X_train, y_train)
-    trainer.run()
-    result = trainer.evaluate(X_test, y_test)
-    trainer.mlflow_log_param('estimator','Linear Regression')
-    trainer.mlflow_log_metric('rsme', result)
+    from sklearn.linear_model import LinearRegression, SGDRegressor
+    estimators = [LinearRegression(),SGDRegressor()]
+    for estimator in estimators:
+        N = 10_000
+        df = get_data(nrows=N)
+        df = clean_data(df)
+        y = df["fare_amount"]
+        X = df.drop("fare_amount", axis=1)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+        trainer = Trainer(X_train, y_train, estimator)
+        trainer.run()
+        trainer.save_model()
